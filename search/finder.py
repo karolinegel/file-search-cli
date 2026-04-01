e9b428c9755d278c111305791ac44693bf34f08a
Karoline Geleisha <154263960+karolinegel@users.noreply.github.com>
2026-04-01 13:07:39 +0300

    Add recursive file search

--- search/finder.py
+++ search/finder.py
@@ -3,3 +3,9 @@ def find_files(path):
     return os.listdir(path)
 def filter_by_extension(files, ext):
     return [f for f in files if f.endswith(ext)]
+def find_files_recursive(path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            result.append(os.path.join(root, file))
+    return result
